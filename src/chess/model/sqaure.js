0173bbe77d0add7e0c358304513a2cd4ec800800
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-09-27 15:37:55 +0530

    chess square class made

---
+++ src/chess/model/sqaure.js
@@ -0,0 +1,13 @@
+class Square{
+    constructor(x,y,canvasCoordinates,piece){
+        // X-Y COORDINATES
+        this.x=x;
+        this.y=y;
+
+        // IN Pixels
+        this.canvasCoordinates=canvasCoordinates;
+
+        // Chess Piece
+        this.piece=piece; 
+    }
+}
