0c3df864b9a465b904377b5fb2fe501842b8351d
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-09-27 20:47:39 +0530

    state and piecemap

---
+++ src/chess/ui/chessgame.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Game from '../model/chess';
+
+// Taking class instead of hooks
+class ChessGame extends React.Component {
+
+    state={
+        // TODO
+        // gameState: new Game(this.props.isWhite),
+    }
+}
+export default class ChessGame extends React.Component {}
