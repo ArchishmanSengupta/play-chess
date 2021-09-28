1f0afcac0023cab60c7fee89d5d794afec9b564d
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-09-28 18:52:28 +0530

    chessgame main logic

--- src/chess/ui/chessgame.js
+++ src/chess/ui/chessgame.js
@@ -2,11 +2,36 @@ import React from "react";
 import Game from '../model/chess';
 
 // Taking class instead of hooks
+// Calculations,pixel coordinates
 class ChessGame extends React.Component {
 
     state={
         // TODO
-        // gameState: new Game(this.props.isWhite),
+        gameState: new Game(this.props.isWhite),
+        // check if white/black king is in check or not
+
+        whiteKingCheck: false,
+        blackKingCheck: false,
+    }
+    // 1. FOR RENDERING
+    render(){
+        return (
+            /***
+             * <div background=wood jpg>
+             *  <stage>
+             *      <layer for loop model and renders it>
+             *</div>
+             */
+        )
+    }
+    // 2. USING PYTHAGOREAN THEOREM 
+    move(selectedId, finalPosition){
+        /**
+         * TO CALCULATE THE distance between the final 
+         *  position of the dragged chess piece for every single square on the board 
+         * and it chooses the closest square (the shortest distance) and it then
+         * assigns that chess piece to that square
+         */
     }
 }
-export default class ChessGame extends React.Component {}
+export default class ChessGame
